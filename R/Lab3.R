c074f3d3cdef2a9d6753896a749c5df1306e499e
Uğurcan Laçin <ugurcanlacin@gmail.com>
2017-09-14 21:34:38 +0200

    Data is fixed

--- R/Lab3.R
+++ R/Lab3.R
@@ -24,15 +24,7 @@ euclidean <- function(m,n){
 #' @title Dijkstra Algorithm Implementation
 #' @name  dijkstra 
 #' @param graph Data Frame of Graph
-#' @param  init_node Numeric Scalar Input
-#' 
-#' @param  previous Numeric Scalar Input
-#' @param  distance Numeric Scalar Input
-#' @param  Q Numeric Scalar Input
-#' @param  u Numeric Scalar Input
-#' @param  dist Numeric Scalar Input
-#' @param  alt Numeric Scalar Input
-#' @param wiki_graph asd
+#' @param init_node Numeric Scalar Input
 #' @return Return a Numeric Vector with the shortest path between nodes in the graph.
 #' @description Dijkstra Algorithm to find the shortest path between nodes in the graph giving a shortest path vector. 
 #' @references \url{https://en.wikipedia.org/wiki/Dijkstra\%27s_algorithm}
@@ -85,10 +77,7 @@ dijkstra <- function(graph, init_node){
 }
 
 #' @title wiki_graph
-#' @name  wiki_graph 
-#' @param v1 A numeric scalar
-#' @param v2 A numeric scalar
-#' @param w A numeric scalar
+#' @name  wiki_graph W
 #' @description wiki_graph
 #' @references \url{https://en.wikipedia.org/wiki/Graph}
 ## wiki_graph
@@ -96,6 +85,6 @@ wiki_graph <-
   data.frame(v1=c(1,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,6),
              v2=c(2,3,6,1,3,4,1,2,4,6,2,3,5,4,6,1,3,5),
              w=c(7,9,14,7,10,15,9,10,11,2,15,11,6,6,9,14,2,9))
-# save(wiki_graph,file="wiki_graph")
+# save(wiki_graph,file="wiki_graph.RData")
 
 aa <- dijkstra(wiki_graph, 1)
