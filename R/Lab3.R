67f91680bd78c6beefed8636f5893ca83fca256d
fahadhameed3h <fahadhameed3h@hotmail.com>
2017-09-14 22:27:34 +0200

    Dataframe

--- R/Lab3.R
+++ R/Lab3.R
@@ -9,7 +9,7 @@ liuid <- "ugula687"
 #' @description Euclidean Algorithm Implementation to find the Greatest Common Divisor(GCD) of two numbers one and two param 
 #' @references \url{https://en.wikipedia.org/wiki/Euclidean_algorithm}
 
-# Euclidean Algorithm
+## Euclidean Algorithm
 euclidean <- function(m,n){
   if(!is.numeric(m) || !is.numeric(n)){
     stop()
