9c376599d4aafdc35cdee14474ff961925150106
Uğurcan Laçin <ugurcanlacin@gmail.com>
2017-09-14 21:39:58 +0200

    Added documentation for data set

--- R/Lab3.R
+++ R/Lab3.R
@@ -79,6 +79,11 @@ dijkstra <- function(graph, init_node){
 #' @title wiki_graph
 #' @name  wiki_graph W
 #' @description wiki_graph
+#' \itemize{
+#'   \item v1 numeric vector
+#'   \item v2 numeric vector
+#'   \item w numeric vector
+#' }
 #' @references \url{https://en.wikipedia.org/wiki/Graph}
 ## wiki_graph
 wiki_graph <-
