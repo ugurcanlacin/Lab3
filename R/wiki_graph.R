95a96adebf11e9950d2c36bb5e401fa008564e3c
Uğurcan Laçin <ugurcanlacin@gmail.com>
2017-09-19 23:56:49 +0200

    changed doc tags

--- R/wiki_graph.R
+++ R/wiki_graph.R
@@ -1,12 +1,16 @@
 #' Wiki_graph
-#'
+#' 
 #' A dataframe that contains the nodes and the weights between them - a simulation of the graph from wikipedia
-#'
-#' @format A data frame with 18 rows and 3 variables:
-#' \describe{
-#'   \item{v1}{node}
-#'   \item{v2}{node}
-#'   \item{w}{weight between v1 and v2 nodes}
+#' 
+#' @title wiki_graph
+#' @name  wiki_graph W
+#' @description wiki_graph
+#' \itemize{
+#'   \item v1 numeric vector
+#'   \item v2 numeric vector
+#'   \item w numeric vector
 #' }
-#' @source \url{https://en.wikipedia.org/wiki/Dijkstra\%27s_algorithm}
+#' @references \url{https://en.wikipedia.org/wiki/Graph}
 "wiki_graph"
+
+
